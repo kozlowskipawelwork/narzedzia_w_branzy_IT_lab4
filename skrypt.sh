81657471f669473d0859bb8f79b0dbb2112f0cd4
Paweł Kozłowski <kozlowskipawelwork@gmail.com>
2026-06-15 18:27:08 +0200

    Dodaj flage --logs (tworzy 100 plikow logX.txt)

--- skrypt.sh
+++ skrypt.sh
@@ -6,9 +6,24 @@ show_date() {
     date
 }
 
+make_logs() {
+    for ((x=1; x<=100; x++)); do
+        plik="log${x}.txt"
+        {
+            echo "Nazwa pliku: $plik"
+            echo "Utworzony przez skrypt: $SCRIPT_NAME"
+            echo "Data utworzenia: $(date)"
+        } > "$plik"
+    done
+    echo "Utworzono 100 plikow log."
+}
+
 case "$1" in
     --date)
         show_date
+        ;;
+    --logs)
+        make_logs
         ;;
     *)
         echo "Nieznana opcja: $1"
